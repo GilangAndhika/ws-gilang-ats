9cc9ba14cbc123914100c12008f9d937fd2f4e26
gryzlegrizz <gryzlegrizz@gmail.com>
2024-05-09 22:54:32 +0700

    Test Package

--- controller/coba.go
+++ controller/coba.go
@@ -4,9 +4,15 @@ import (
     "github.com/aiteung/musik"
     "github.com/gofiber/fiber/v2"
 
+	cek "github.com/gryzlegrizz/package_uts/module"
 )
 
 func Homepage(c *fiber.Ctx) error {
     ipaddr := musik.GetIPaddress()
     return c.JSON(ipaddr)
 }
+
+func GetAllMuseumCollections(c *fiber.Ctx) error {
+	ps := cek.GetAllMuseumCollections()
+	return c.JSON(ps)
+	}
